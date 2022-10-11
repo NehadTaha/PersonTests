55b0623c14af3ac44d32aeabad100076a48db652
Jeremy Blackburn <LE2152908@crc-lennox.qc.ca>
2022-10-11 10:17:33 -0400

    Revert "Final Changes"
    This reverts commit f638ae89e69a80c67c12725faf0f805175ab69e0.

--- PersonTests/Person.cs
+++ PersonTests/Person.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents one person saving their name.
     /// </summary>
-    public class Person : IDisplay, IComparable<Person>
+    public class Person
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Person"/> class.
@@ -27,24 +27,6 @@
         public override string ToString()
         {
             return this.Name;
-        }
-
-        /// <summary>
-        /// Displays name.
-        /// </summary>
-        public void Display()
-        {
-            MessageBox.Show(this.Name);
-        }
-
-        /// <summary>
-        /// Compares the names of the person.
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns>the int value of compareTo method</returns>
-        public int compareTo(Person other)
-        {
-            return this.Name.CompareTo(other.Name);
         }
     }
 }
